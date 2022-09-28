06d31a5459cd9fb4e2ce799ff57cbd6cbdd70862
AliJonMadaminov <aliufc2004@gmail.com>
2022-09-28 18:18:54 +0500

    backstack manipulation is done

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -22,17 +22,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_over, container, false)
-        // TODO (05) Add an onClick Handler for the tryAgain button that navigates to the gameFragment
-        // using action_gameOverFragment_to_gameFragment
+            inflater, R.layout.fragment_game_over, container, false
+        )
+        binding.tryAgainButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_gameOverFragment_to_gameFragment)
+        )
+
         return binding.root
     }
 }
--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -33,17 +33,19 @@ import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 import android.content.pm.ResolveInfo
 import android.content.pm.PackageManager
-
+import androidx.navigation.Navigation
 
 
 class GameWonFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
-        // TODO (06) Add an onClick Handler for the nextMatch button that navigates to the gameFragment
-        // using action_gameWonFragment_to_gameFragment
+        binding.nextMatchButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_gameWonFragment_to_gameFragment)
+        )
+
         return binding.root
     }
 }
